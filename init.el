d2e77de80307796f0af2f5d34913e7b6246d0e60
ebresafegaga <ebresafegaga@outlook.com>
2021-06-19 14:15:28 +0100

    enabled scroll bar mode

--- init.el
+++ init.el
@@ -6,7 +6,7 @@
 	     '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
-;(scroll-bar-mode -1)
+(scroll-bar-mode -1)
 ;(menu-bar-mode -1)
 ;(tool-bar-mode -1)
 (custom-set-variables
